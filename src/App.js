bfcde24827f1f9a6864ee1c9d294de0a50db191b
aiden-riddler <seletemoses@gmail.com>
2024-04-30 21:42:02 +0300

    Add address and wallet view

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
+import { useTonAddress } from '@tonconnect/ui-react';
+import { useTonWallet } from '@tonconnect/ui-react';
 
 function App() {
+  const userFriendlyAddress = useTonAddress();
+    const rawAddress = useTonAddress(false);
   return (
 
     <TonConnectUIProvider manifestUrl="https://sweet-heart-7938.on.fleek.co/tonconnect-manifest.json">
        <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TonConnectUIProviderAdded
-        </p>
         <span>My App with React UI</span>
             <TonConnectButton />
+            <div>Address</div>
+            userFriendlyAddress && (
+            <div>
+                <span>User-friendly address: {userFriendlyAddress}</span>
+                <span>Raw address: {rawAddress}</span>
+            </div>
+
+            <div>Wallet</div>
+            wallet && (
+            <div>
+                <span>Connected wallet: {wallet.name}</span>
+                <span>Device: {wallet.device.appName}</span>
+            </div>
+        )
+        )
       </header>
     </div>
     </TonConnectUIProvider>
