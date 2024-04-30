f08af604240f1db0f63f9c55716c5d58b42b1155
aiden-riddler <seletemoses@gmail.com>
2024-04-30 21:45:40 +0300

    correct update

--- src/App.js
+++ src/App.js
@@ -6,31 +6,30 @@ import { useTonWallet } from '@tonconnect/ui-react';
 
 function App() {
   const userFriendlyAddress = useTonAddress();
-    const rawAddress = useTonAddress(false);
+  const rawAddress = useTonAddress(false);
+  const wallet = useTonWallet();
   return (
-
     <TonConnectUIProvider manifestUrl="https://sweet-heart-7938.on.fleek.co/tonconnect-manifest.json">
-       <div className="App">
-      <header className="App-header">
-        <span>My App with React UI</span>
-            <TonConnectButton />
-            <div>Address</div>
-            userFriendlyAddress && (
-            <div>
-                <span>User-friendly address: {userFriendlyAddress}</span>
-                <span>Raw address: {rawAddress}</span>
-            </div>
-
-            <div>Wallet</div>
-            wallet && (
-            <div>
-                <span>Connected wallet: {wallet.name}</span>
-                <span>Device: {wallet.device.appName}</span>
-            </div>
-        )
-        )
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <span>My App with React UI Update</span>
+          <TonConnectButton />
+          <div>Address</div>
+          userFriendlyAddress && (
+          <div>
+            <span>User-friendly address: {userFriendlyAddress}</span>
+            <span>Raw address: {rawAddress}</span>
+          </div>
+          )
+          <div>Wallet</div>
+          wallet && (
+          <div>
+            <span>Connected wallet: {wallet.name}</span>
+            <span>Device: {wallet.device.appName}</span>
+          </div>
+          )
+        </header>
+      </div>
     </TonConnectUIProvider>
   );
 }
